9bdf1ba030b9a5dedf3d1e1e8f89fc5e90a0dc4c
Christopher Siems <chris.siems@vevo.com>
2022-12-04 14:03:07 -0800

    transparent round calculations

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -2,30 +2,30 @@ fun playRockPaperScissors(input: List<Pair<Char,Char>>, calculator: (Pair<Char,C
 
 fun round1Calculator(roundResult: Pair<Char,Char>): Int {
     return when(roundResult) {
-        Pair('A','X') -> 4
-        Pair('A','Y') -> 8
-        Pair('A','Z') -> 3
-        Pair('B','X') -> 1
-        Pair('B','Y') -> 5
-        Pair('B','Z') -> 9
-        Pair('C','X') -> 7
-        Pair('C','Y') -> 2
-        Pair('C','Z') -> 6
+        Pair('A','X') -> 1 + 3
+        Pair('A','Y') -> 2 + 6
+        Pair('A','Z') -> 3 + 0
+        Pair('B','X') -> 1 + 0
+        Pair('B','Y') -> 2 + 3
+        Pair('B','Z') -> 3 + 6
+        Pair('C','X') -> 1 + 6
+        Pair('C','Y') -> 2 + 0
+        Pair('C','Z') -> 3 + 3
         else -> throw Exception("malformed input")
     }
 }
 
 fun round2Calculator(roundResult: Pair<Char,Char>): Int {
     return when(roundResult) {
-        Pair('A','X') -> 3
-        Pair('A','Y') -> 4
-        Pair('A','Z') -> 8
-        Pair('B','X') -> 1
-        Pair('B','Y') -> 5
-        Pair('B','Z') -> 9
-        Pair('C','X') -> 2
-        Pair('C','Y') -> 6
-        Pair('C','Z') -> 7
+        Pair('A','X') -> 3 + 0
+        Pair('A','Y') -> 1 + 3
+        Pair('A','Z') -> 2 + 6
+        Pair('B','X') -> 1 + 0
+        Pair('B','Y') -> 2 + 3
+        Pair('B','Z') -> 3 + 6
+        Pair('C','X') -> 2 + 0
+        Pair('C','Y') -> 3 + 3
+        Pair('C','Z') -> 1 + 6
         else -> throw Exception("malformed input")
     }
 }
