78f8342182105b695c6172e9621d702246dabd1b
Christopher Siems <chris.siems@vevo.com>
2022-12-05 09:55:54 -0800

    shamelessly steal stuff from todd ginsberg

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -2,67 +2,49 @@ fun useCrane9000(input: String): String {
     val stacks = input.extractStackedBoxes()
     val commands = input.extractMoveCommands()
 
-    return stacks.processCrane9000MoveCommands(commands).toTopLine()
+    return stacks.processMoveCommands(commands, true).toTopLine()
 }
 
 fun useCrane9001(input: String): String {
     val stacks = input.extractStackedBoxes()
     val commands = input.extractMoveCommands()
 
-    return stacks.processCrane9001MoveCommands(commands).toTopLine()
+    return stacks.processMoveCommands(commands, false).toTopLine()
 }
 
 private data class Command(val amount: Int, val from: Int, val to: Int)
 
-private fun List<ArrayDeque<Char>>.processCrane9000MoveCommands(commands: List<Command>): List<ArrayDeque<Char>> {
-    commands.forEach { this.crane9000Move(it) }
+private fun List<ArrayDeque<Char>>.processMoveCommands(commands: List<Command>, reverse: Boolean): List<ArrayDeque<Char>> {
+    commands.forEach { this.move(it, reverse) }
     return this
 }
 
-private fun List<ArrayDeque<Char>>.crane9000Move(command: Command) {
+private fun List<ArrayDeque<Char>>.move(command: Command, reverse: Boolean) {
+    val boxesToMove = ArrayDeque<Char>()
     for (i in 1 .. command.amount) {
         val box = this[command.from - 1].removeFirst()
-        this[command.to - 1].addFirst(box)
-    }
-}
-
-private fun List<ArrayDeque<Char>>.processCrane9001MoveCommands(commands: List<Command>): List<ArrayDeque<Char>> {
-    commands.forEach { this.crane9001Move(it) }
-    return this
-}
-
-private fun List<ArrayDeque<Char>>.crane9001Move(command: Command) {
-    val tempDeque = ArrayDeque<Char>()
-    for (i in 1 .. command.amount) {
-        val box = this[command.from - 1].removeFirst()
-        tempDeque.addLast(box)
+        boxesToMove.addLast(box)
     }
 
-    for (i in 1..tempDeque.size) {
-        val inflight = tempDeque.removeLast()
-        this[command.to - 1].addFirst(inflight)
+    for (i in 1..boxesToMove.size) {
+        val inFlight = if (reverse) boxesToMove.removeFirst() else boxesToMove.removeLast()
+        this[command.to - 1].addFirst(inFlight)
     }
-
 }
 
 private fun List<ArrayDeque<Char>>.toTopLine() = this.map { it.first() }.joinToString("")
 
 private fun String.extractStackedBoxes(): List<ArrayDeque<Char>> {
-    val boxRows =  this.substringBefore("\n\n").lines().map { it.chunked(4) }
-    val columns = boxRows.maxByOrNull { it.size }?.map { ArrayDeque<Char>(listOf()) } ?: emptyList()
-
-    boxRows.forEach { row ->
-        row.forEachIndexed { index, box ->
-            box.trim()
-                .let { boxString ->
-                    if (boxString.isNotBlank() && boxString.all { !it.isDigit() }) {
-                        val boxChar = boxString.filter { it.isLetter() }.toCharArray().single()
-                        columns[index].addLast(boxChar)
-                    }
-                }
-        }
+    val rows =  this.substringBefore("\n\n").lines()
+    return (1..rows.last().length step 4).map { index ->
+        rows
+            .also { println("Rows: $it") }
+            .mapNotNull { it.getOrNull(index) }
+            .also { println("Mapped Not null: $it") }
+            .filter { it.isUpperCase() }
+            .also { println("filtered is uppercase: $it") }
+            .let { ArrayDeque(it) }
     }
-    return columns
 }
 
 private fun String.extractMoveCommands() = this.substringAfter("\n\n").trim().lines().map { it.toCommand() }
