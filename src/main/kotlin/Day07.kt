b02d6a02aced19bbbedcac6c50f0e5383cad8dff
Christopher Siems <chris.siems@vevo.com>
2022-12-06 23:51:01 -0800

    day 7

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,3 +1,69 @@
-fun doStuff(input: Any): Int {
-    return 0
+
+fun sumSmallDirectories(input: List<String>): Long {
+    val allDirectories = parseCommands(input)
+    return allDirectories.filter { it.size <= 100_000 }.sumOf { it.size }
+}
+
+fun findSmallestDirectoryNecessary(input: List<String>): Long {
+    val allDirectories = parseCommands(input).sortedBy { it.size }
+    val memoryCurrentlyAvailable = 70000000L - allDirectories.last().size
+    val memoryNeeded = 30000000L
+    return allDirectories.first { it.size >= (memoryNeeded - memoryCurrentlyAvailable) }.size
+}
+
+private fun parseCommands(input: List<String>): MutableSet<ElfDirectory> {
+
+    val rootDirectory = ElfDirectory("/", null)
+    val allDirectories = mutableSetOf<ElfDirectory>().also { it.add(rootDirectory) }
+
+    var currentDirectory = rootDirectory
+
+    input.forEachIndexed { index, line ->
+        val parts = line.split(" ")
+        val command = parts[1]
+        val location by lazy { parts[2] }
+
+        when (command) {
+            "ls" -> {
+                var nextIndex = index + 1
+                while (nextIndex < input.size && input[nextIndex][0] != '$') {
+                    val item = input[nextIndex]
+                    item.split(" ").let { (sizeOrDir, name) ->
+                        when (sizeOrDir) {
+                            "dir" -> {
+                                // add to all directories and as subdirectory to current
+                                val directory = ElfDirectory(name, currentDirectory)
+                                allDirectories.add(directory)
+                                currentDirectory.directories.putIfAbsent(name, directory)
+                            }
+                            else -> currentDirectory.files.putIfAbsent(name, ElfFile(name, sizeOrDir.toLong()))
+                        }
+                    }
+                    nextIndex++
+                }
+            }
+            "cd" -> {
+                currentDirectory = when (location) {
+                    "/" -> rootDirectory
+                    ".." -> currentDirectory.parent!!
+                    else -> currentDirectory.directories[location]!!
+                }
+            }
+            else -> {
+                // do nothing with files because we've already added them
+            }
+        }
+    }
+    return allDirectories
+}
+
+data class ElfFile(val name: String, val size: Long)
+
+data class ElfDirectory(val name: String, val parent: ElfDirectory?) {
+    val files = mutableMapOf<String, ElfFile>()
+    val directories = mutableMapOf<String, ElfDirectory>()
+
+    val size: Long by lazy {
+        files.values.sumOf { it.size } + directories.values.sumOf { it.size }
+    }
 }
--- src/test/kotlin/Day07Test.kt
+++ src/test/kotlin/Day07Test.kt
@@ -2,30 +2,54 @@ import io.kotest.matchers.shouldBe
 import org.testng.annotations.Test
 
 class Day07Test {
-    private val sampleData = emptyList<String>()
-    private val actualData = getFile("Day07").readLines()
+    private val sampleData = listOf(
+        "$ cd /",
+        "$ ls",
+        "dir a",
+        "14848514 b.txt",
+        "8504156 c.dat",
+        "dir d",
+        "$ cd a",
+        "$ ls",
+        "dir e",
+        "29116 f",
+        "2557 g",
+        "62596 h.lst",
+        "$ cd e",
+        "$ ls",
+        "584 i",
+        "$ cd ..",
+        "$ cd ..",
+        "$ cd d",
+        "$ ls",
+        "4060174 j",
+        "8033020 d.log",
+        "5626152 d.ext",
+        "7214296 k"
+    ).drop(1)
+    private val actualData = getFile("Day07").readLines().drop(1)
 
     @Test
     fun `part 1 sample`() {
-        val result = doStuff(sampleData)
-        result shouldBe 0
+        val result = sumSmallDirectories(sampleData)
+        result shouldBe 95437L
     }
 
     @Test
     fun `part 1`() {
-        val result = doStuff(actualData)
-        result shouldBe 0
+        val result = sumSmallDirectories(actualData)
+        result shouldBe 1367870L
     }
 
     @Test
     fun `part 2 sample`() {
-        val result = doStuff(sampleData)
-        result shouldBe 0
+        val result = findSmallestDirectoryNecessary(sampleData)
+        result shouldBe 24933642
     }
 
     @Test
     fun `part 2`() {
-        val result = doStuff(actualData)
-        result shouldBe 0
+        val result = findSmallestDirectoryNecessary(actualData)
+        result shouldBe 549173
     }
 }
