3791628b8b13ae4099bd04748676aec2f9768392
Christopher Siems <chris.siems@vevo.com>
2022-12-08 14:31:23 -0800

    add some extensions

---
+++ src/main/kotlin/shared/Extensions.kt
@@ -0,0 +1,15 @@
+package shared
+
+// Modified version of takeWhile from the Kotlin Standard Library
+inline fun <T> Iterable<T>.takeUntil(predicate: (T) -> Boolean): List<T> {
+    val list = ArrayList<T>()
+    for (item in this) {
+        list.add(item)
+        if (predicate(item))
+            break
+    }
+    return list
+}
+
+fun Iterable<Int>.product(): Int =
+    reduce { a, b -> a * b }
