89b843cee198a3d949067ed6cb58d7e4917e3bdf
Christopher Siems <chris.siems@vevo.com>
2022-12-08 23:38:11 -0800

    day 9

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,3 +1,190 @@
-fun doStuff(input: List<String>): Int {
-    return 0
+import shared.Point2d
+
+fun countTwoKnotRopeTailPositions(input: List<String>): Int {
+    clearFields()
+    input.forEach { processTwoKnotCommand(it) }
+    return pastTailPositions.count()
+}
+
+fun countTenKnotRopeTailPositions(input: List<String>): Int {
+    clearFields()
+    input.forEach { processTenKnotCommand(it) }
+    return pastTailPositions.count()
+}
+
+var pastTailPositions = mutableSetOf(Point2d(0,0))
+var currentHeadPosition = Point2d(0,0)
+var current1Position = Point2d(0,0)
+var current2Position = Point2d(0,0)
+var current3Position = Point2d(0,0)
+var current4Position = Point2d(0,0)
+var current5Position = Point2d(0,0)
+var current6Position = Point2d(0,0)
+var current7Position = Point2d(0,0)
+var current8Position = Point2d(0,0)
+var currentTailPosition = Point2d(0,0)
+
+fun processTwoKnotCommand(command: String) {
+    val (direction, times) = command.split(" ")
+    when(direction) {
+        "R" -> {
+            repeat(times.toInt()) {
+                currentHeadPosition = move(currentHeadPosition, Point2d(1,0))
+                currentTailPosition = moveFollower(currentHeadPosition, currentTailPosition, true)
+            }
+        }
+        "L" -> {
+            repeat(times.toInt()) {
+                currentHeadPosition = move(currentHeadPosition, Point2d(-1,0))
+                currentTailPosition = moveFollower(currentHeadPosition, currentTailPosition, true)
+            }
+        }
+        "U" -> {
+            repeat(times.toInt()) {
+                currentHeadPosition = move(currentHeadPosition, Point2d(0,1))
+                currentTailPosition = moveFollower(currentHeadPosition, currentTailPosition, true)
+            }
+        }
+        "D" -> {
+            repeat(times.toInt()) {
+                currentHeadPosition = move(currentHeadPosition, Point2d(0,-1))
+                currentTailPosition = moveFollower(currentHeadPosition, currentTailPosition, true)
+            }
+        }
+        else -> throw IllegalArgumentException("Bad command in instructions")
+    }
+}
+
+fun processTenKnotCommand(command: String) {
+    val (direction, times) = command.split(" ")
+    when(direction) {
+        "R" -> {
+            repeat(times.toInt()) {
+                currentHeadPosition = move(currentHeadPosition, Point2d(1,0))
+                moveTenKnots()
+            }
+        }
+        "L" -> {
+            repeat(times.toInt()) {
+                currentHeadPosition = move(currentHeadPosition, Point2d(-1,0))
+                moveTenKnots()
+            }
+        }
+        "U" -> {
+            repeat(times.toInt()) {
+                currentHeadPosition = move(currentHeadPosition, Point2d(0,1))
+                moveTenKnots()
+            }
+        }
+        "D" -> {
+            repeat(times.toInt()) {
+                currentHeadPosition = move(currentHeadPosition, Point2d(0,-1))
+                moveTenKnots()
+            }
+        }
+        else -> throw IllegalArgumentException("Bad command in instructions")
+    }
+}
+
+fun moveTenKnots() {
+    current1Position = moveFollower(currentHeadPosition, current1Position)
+    current2Position = moveFollower(current1Position, current2Position)
+    current3Position = moveFollower(current2Position, current3Position)
+    current4Position = moveFollower(current3Position, current4Position)
+    current5Position = moveFollower(current4Position, current5Position)
+    current6Position = moveFollower(current5Position, current6Position)
+    current7Position = moveFollower(current6Position, current7Position)
+    current8Position = moveFollower(current7Position, current8Position)
+    currentTailPosition = moveFollower(current8Position, currentTailPosition, true)
+}
+
+
+fun moveFollower(currentLeadingPosition: Point2d, currentFollowingPosition: Point2d, trackChanges: Boolean = false): Point2d {
+
+    val diff = currentLeadingPosition.diff(currentFollowingPosition)
+
+    val newFollowingPosition = if (diff.y == 2) {
+        // head is above tail
+        if (diff.x == 0) {
+            currentFollowingPosition.moveNorth()
+        } else if (diff.x >= 1) {
+            currentFollowingPosition.moveNortheast()
+        } else {
+            currentFollowingPosition.moveNorthwest()
+        }
+    } else if (diff.y == -2) {
+        // head is below tail
+        if (diff.x == 0) {
+            currentFollowingPosition.moveSouth()
+        } else if (diff.x >= 1) {
+            currentFollowingPosition.moveSoutheast()
+        } else {
+            currentFollowingPosition.moveSouthwest()
+        }
+    } else if (diff.x == -2) {
+        // head is left
+        if (diff.y == 0) {
+            currentFollowingPosition.moveWest()
+        } else if (diff.y >= 1) {
+            currentFollowingPosition.moveNorthwest()
+        } else {
+            currentFollowingPosition.moveSouthwest()
+        }
+    } else if (diff.x == 2) {
+        // head is right
+        if (diff.y == 0) {
+            currentFollowingPosition.moveEast()
+        } else if (diff.y >= 1) {
+            currentFollowingPosition.moveNortheast()
+        } else {
+            currentFollowingPosition.moveSoutheast()
+        }
+    } else {
+        // no need to move tail
+        currentFollowingPosition
+    }
+    if (trackChanges) {
+        pastTailPositions.add(newFollowingPosition)
+    }
+    return newFollowingPosition
+}
+
+
+fun Point2d.moveNorth() = move(this, Point2d(0, 1))
+fun Point2d.moveSouth() = move(this, Point2d(0, -1))
+fun Point2d.moveWest() = move(this, Point2d(-1, 0))
+fun Point2d.moveEast() = move(this, Point2d(1, 0))
+fun Point2d.moveNortheast() = move(this, Point2d(1, 1))
+fun Point2d.moveNorthwest() = move(this, Point2d(-1, 1))
+fun Point2d.moveSoutheast() = move(this, Point2d(1, -1))
+fun Point2d.moveSouthwest() = move(this, Point2d(-1, -1))
+
+fun move(currentLocation: Point2d, relativeLocation: Point2d): Point2d {
+    return currentLocation + relativeLocation
+}
+
+private fun Point2d.diff(other: Point2d): Point2d {
+    return this - other
+}
+
+private operator fun Point2d.plus(other: Point2d): Point2d {
+    return Point2d(this.x + other.x, this.y + other.y)
+}
+
+private operator fun Point2d.minus(other: Point2d): Point2d {
+    return Point2d(this.x - other.x, this.y - other.y)
+}
+
+fun clearFields() {
+    pastTailPositions = mutableSetOf(Point2d(0,0))
+    currentHeadPosition = Point2d(0,0)
+    current1Position = Point2d(0,0)
+    current2Position = Point2d(0,0)
+    current3Position = Point2d(0,0)
+    current4Position = Point2d(0,0)
+    current5Position = Point2d(0,0)
+    current6Position = Point2d(0,0)
+    current7Position = Point2d(0,0)
+    current8Position = Point2d(0,0)
+    currentTailPosition = Point2d(0,0)
 }
--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -2,30 +2,55 @@ import io.kotest.matchers.shouldBe
 import org.testng.annotations.Test
 
 class Day09Test {
-    private val sampleData = listOf("")
+    private val sampleData = listOf(
+        "R 4",
+        "U 4",
+        "L 3",
+        "D 1",
+        "R 4",
+        "D 1",
+        "L 5",
+        "R 2"
+    )
+    private val largerSampleData = listOf(
+        "R 5",
+        "U 8",
+        "L 8",
+        "D 3",
+        "R 17",
+        "D 10",
+        "L 25",
+        "U 20"
+    )
     private val actualData = getFile("Day09").readLines()
 
     @Test
     fun `part 1 sample`() {
-        val result = doStuff(sampleData)
-        result shouldBe 0
+        val result = countTwoKnotRopeTailPositions(sampleData)
+        result shouldBe 13
     }
 
     @Test
     fun `part 1`() {
-        val result = doStuff(actualData)
-        result shouldBe 0
+        val result = countTwoKnotRopeTailPositions(actualData)
+        result shouldBe 5874
     }
 
     @Test
     fun `part 2 sample`() {
-        val result = doStuff(sampleData)
-        result shouldBe 0
+        val result = countTenKnotRopeTailPositions(sampleData)
+        result shouldBe 1
+    }
+
+    @Test
+    fun `part 2 larger sample`() {
+        val result = countTenKnotRopeTailPositions(largerSampleData)
+        result shouldBe 36
     }
 
     @Test
     fun `part 2`() {
-        val result = doStuff(actualData)
-        result shouldBe 0
+        val result = countTenKnotRopeTailPositions(actualData)
+        result shouldBe 2467
     }
 }
