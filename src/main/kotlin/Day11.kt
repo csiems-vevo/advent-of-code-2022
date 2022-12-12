3a454bb22ee0747e392672fa24d0961716f94204
Christopher Siems <chris.siems@vevo.com>
2022-12-11 17:18:45 -0800

    day 11

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,3 +1,84 @@
-fun doStuff(input: List<String>): Int {
-    return 0
+import shared.product
+
+fun runMonkeyProgram(rounds: Int, input: List<String>, stressModifier:((value: Long) -> Long)? = null): Long {
+
+    val monkeys = input.map { it.lines() }
+        .map { group -> Monkey.of(group) }
+
+
+    repeat(rounds) {
+        monkeys.forEach { monkey ->
+            if (stressModifier != null) {
+                monkey.inspectItems(monkeys) { stressModifier(it)}
+            } else {
+                val divisor = monkeys.map { it.divisible }.product()
+                monkey.inspectItems(monkeys) { it % divisor }
+            }
+        }
+    }
+
+    return monkeys.sortedByDescending { it.itemsInspected }
+        .take(2)
+        .map { it.itemsInspected }
+        .product()
+}
+
+data class Monkey(
+    val startingItems: MutableList<Item>,
+    val worryLevelTransformer: (old: Long) -> Long,
+    val divisible: Long,
+    val itemThrowDecision: (old: Long) -> Int
+) {
+
+    var itemsInspected = 0L
+
+    companion object {
+        fun of(inputGroup: List<String>): Monkey {
+            val startingItems = inputGroup[1].split(" ",",")
+                .filterNot { it.isEmpty() }
+                .drop(2)
+                .map { Item(it.toLong()) }
+                .toMutableList()
+            val worryLevelTransformer = extractWorryLevelTransformer(inputGroup[2])
+
+            val divisible = inputGroup[3].split(" ").last().toLong()
+            val trueCondition = inputGroup[4].split(" ").last().toInt()
+            val falseCondition = inputGroup[5].split(" ").last().toInt()
+            val itemThrowDecision = buildThrowDecision(divisible, trueCondition, falseCondition)
+
+            return Monkey(startingItems, worryLevelTransformer, divisible, itemThrowDecision)
+        }
+    }
+
+    fun inspectItems(monkeys: List<Monkey>, reliefTransformer: (Long) -> Long) {
+        startingItems.forEach { item ->
+            val newWorryLevel = reliefTransformer(worryLevelTransformer(item.worryLevel))
+            val target = itemThrowDecision(newWorryLevel)
+            monkeys[target].startingItems.add(Item(newWorryLevel))
+        }
+        itemsInspected += startingItems.size
+        startingItems.clear()
+    }
+}
+
+data class Item(val worryLevel: Long)
+
+private fun buildThrowDecision(divisible: Long, trueCondition: Int, falseCondition: Int): (Long) -> Int {
+
+    return fun(old: Long): Int {
+        return if ((old % divisible) == 0L) {
+            trueCondition
+        } else {
+            falseCondition
+        }
+    }
+}
+
+private fun extractWorryLevelTransformer(input: String): (Long) -> Long {
+    val parts = input.split(" ")
+    val value = parts.last().toLongOrNull() ?: return fun(old: Long): Long { return old * old}
+    return when {
+        parts.dropLast(1).last() == "*" -> fun(old: Long): Long { return old * value}
+        else -> fun(old: Long): Long { return old + value}
+    }
 }
--- src/test/kotlin/Day11Test.kt
+++ src/test/kotlin/Day11Test.kt
@@ -2,30 +2,59 @@ import io.kotest.matchers.shouldBe
 import org.testng.annotations.Test
 
 class Day11Test {
-    private val sampleData = listOf<String>()
-    private val actualData = getFile("Day11").readLines()
+    private val sampleData = """
+            Monkey 0:
+              Starting items: 79, 98
+              Operation: new = old * 19
+              Test: divisible by 23
+                If true: throw to monkey 2
+                If false: throw to monkey 3
+            
+            Monkey 1:
+              Starting items: 54, 65, 75, 74
+              Operation: new = old + 6
+              Test: divisible by 19
+                If true: throw to monkey 2
+                If false: throw to monkey 0
+            
+            Monkey 2:
+              Starting items: 79, 60, 97
+              Operation: new = old * old
+              Test: divisible by 13
+                If true: throw to monkey 1
+                If false: throw to monkey 3
+            
+            Monkey 3:
+              Starting items: 74
+              Operation: new = old + 3
+              Test: divisible by 17
+                If true: throw to monkey 0
+                If false: throw to monkey 1
+        """.trimIndent().splitByNewLine()
+
+    private val actualData = getFile("Day11").splitByNewline()
 
     @Test
     fun `part 1 sample`() {
-        val result = doStuff(sampleData)
-        result shouldBe 0
+        val result = runMonkeyProgram(20, sampleData, fun(value: Long): Long { return value / 3 })
+        result shouldBe 10605
     }
 
     @Test
     fun `part 1`() {
-        val result = doStuff(actualData)
-        result shouldBe 0
+        val result = runMonkeyProgram(20, actualData, fun(value: Long): Long { return value / 3 })
+        result shouldBe 182293
     }
 
     @Test
     fun `part 2 sample`() {
-        val result = doStuff(sampleData)
-        result shouldBe 0
+        val result = runMonkeyProgram(10_000, sampleData)
+        result shouldBe 2713310158
     }
 
     @Test
     fun `part 2`() {
-        val result = doStuff(actualData)
-        result shouldBe 0
+        val result = runMonkeyProgram(10_000, actualData)
+        result shouldBe 54832778815
     }
 }
