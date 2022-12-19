9bf32d998c11510afdbbe1be17df3249bade914b
Christopher Siems <chris.siems@vevo.com>
2022-12-19 07:15:13 -0800

    add guava

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.guava:guava:31.1-jre")
     testImplementation(kotlin("test"))
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
